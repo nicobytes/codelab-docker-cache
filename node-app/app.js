cb90cd9aca0d217aaaee9fc067c72f6153226d9b
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2019-04-21 15:35:20 -0400

    🚧 work

--- node-app/app.js
+++ node-app/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send('Hello World!123');
 });
 
 app.listen(3000, function () {
---
+++ node-app/dev.dockerfile
@@ -0,0 +1,11 @@
+FROM node:11.14.0
+
+WORKDIR /app
+
+COPY package.json package-lock.json /app/
+RUN npm install
+
+EXPOSE 3000
+COPY . /app/
+
+CMD npm run start:dev
